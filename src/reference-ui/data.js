c45eea437df3ec2eed834de6a23afef165e39bb5
Jairo Franco Garduño <jairofrancog7@gmail.com>
2026-09-17 14:15:39 -0600

    chore: add local reference UI data registry

---
+++ src/reference-ui/data.js
@@ -0,0 +1,19 @@
+export const LEAGUE_LOGO='https://d2ol7oe51mr4n9.cloudfront.net/user_3JNvttsAwr0QjxhuX5O1uaa9bvv/720d1f82-1d59-4a7a-af69-9e7e8da0250a.png';
+export const TEAMS=[
+{id:'america',code:'AME',name:'Club América Vet.',short:'América',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/branding/america-veteranos-35-user.png',p:1,gd:5,pts:3,form:['n','n','w']},
+{id:'huerta',code:'HUE',name:'La Huerta',short:'La Huerta',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/la-huerta-cuenda.webp',p:1,gd:5,pts:3,form:['n','n','w']},
+{id:'promesas',code:'PRO',name:'Promesas FC',short:'Promesas',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/promesas-fc-pozos.webp',p:1,gd:4,pts:3,form:['n','n','w']},
+{id:'franco',code:'FCO',name:'Franco FC',short:'Franco FC',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/franco-fc.webp',p:1,gd:4,pts:3,form:['n','n','w']},
+{id:'galeana',code:'ATG',name:'Atlético Galeana',short:'Atl. Galeana',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/atletico-galeana.webp',p:1,gd:3,pts:3,form:['n','n','w']},
+{id:'lobos',code:'LOB',name:'Lobos CDG',short:'Lobos',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/lobos-cdg.webp',p:1,gd:2,pts:3,form:['n','n','w']},
+{id:'juventino',code:'JUV',name:'Juventino',short:'Juventino',logo:LEAGUE_LOGO,p:1,gd:2,pts:3,form:['n','n','w']},
+{id:'pozos',code:'POZ',name:'Pozos FC',short:'Pozos',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/pozos-fc.webp',p:1,gd:1,pts:3,form:['n','d','w']},
+{id:'cuenda',code:'CUE',name:'Cuenda',short:'Cuenda',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/tc-cuenda.webp',p:1,gd:1,pts:1,form:['n','d','d']},
+{id:'esperanza',code:'ESP',name:'La Esperanza',short:'Esperanza',logo:'https://raw.githubusercontent.com/jairofrancog7-star/Liga_Futbol/main/assets/teams/la-esperanza-fc.webp',p:1,gd:0,pts:1,form:['n','d','d']}
+];
+export const TEAM=id=>TEAMS.find(t=>t.id===id)||TEAMS[0];
+export const FIXTURES={
+'10 sept':[['america','huerta','10:45'],['promesas','franco','10:45'],['galeana','lobos','13:00'],['juventino','pozos','13:00']],
+'13 oct':[['america','huerta','10:45'],['promesas','franco','10:45'],['galeana','lobos','13:00'],['juventino','pozos','13:00'],['huerta','franco','13:00'],['promesas','america','13:00'],['lobos','cuenda','13:00']],
+'14 oct':[['pozos','galeana','10:45'],['franco','america','13:00'],['huerta','lobos','13:00'],['cuenda','promesas','13:00']]
+};
